78cee521d0a65c5a0a9b409efe4292a30021cb6a
gotnull <fcusumano@gmail.com>
2021-07-09 14:37:27 +1000

    2 files added

---
+++ packages/backend/src/api/index.js
@@ -0,0 +1,21 @@
+import './db';
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+import { services } from '../services';
+
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// Mount REST on /api
+app.use('/api', services);
+
+//const port = process.env.PORT;
+//app.listen(port, () => console.log(`Express app listening on: ${port}`));
+
+module.exports = app;
