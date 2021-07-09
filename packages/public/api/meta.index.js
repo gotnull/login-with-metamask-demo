08d5fd5408ea3514d6c5bc1b8ae8f96c4ed3341b
gotnull <fcusumano@gmail.com>
2021-07-09 15:41:54 +1000

    1 file modified

--- packages/public/api/meta.index.js
+++ packages/public/api/meta.index.js
@@ -15,7 +15,4 @@ app.use(cors());
 // Mount REST on /api
 app.use('/api', services);
 
-//const port = process.env.PORT;
-//app.listen(port, () => console.log(`Express app listening on: ${port}`));
-
 module.exports = app;
