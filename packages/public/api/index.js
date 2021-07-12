73a4d86c87a733ca4b18877c1610eb64deeafcf2
gotnull <fcusumano@gmail.com>
2021-07-12 15:13:06 +1000

    1 file modified, 27 added, 3 deleted

--- packages/public/api/index.js
+++ packages/public/api/index.js
@@ -1,16 +1,18 @@
-const app = require('express')();
-const { v4 } = require('uuid');
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./db");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const services_1 = require("./services");
+const app = express_1.default();
+// Middlewares
+app.use(body_parser_1.default.json());
+app.use(cors_1.default());
+// Mount REST on /api
+app.use('/api', services_1.services);
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Express app listening on localhost:${port}`));
