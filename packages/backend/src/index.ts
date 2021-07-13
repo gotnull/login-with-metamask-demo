862977d5eab444b7d06dff7c8c64533397f53131
gotnull <fcusumano@gmail.com>
2021-07-13 18:09:27 +1000

    3 files modified

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,9 +16,3 @@ app.use(cors());
 app.use('/api', services);
 
 module.exports = app;
-
-// const port = process.env.PORT || 8000;
-
-// app.listen(port, () =>
-// 	console.log(`Express app listening on localhost:${port}`)
-// );
