b85e08655e1ce8b496cf8b57c954b137bdf509e2
gotnull <fcusumano@gmail.com>
2021-07-14 14:49:14 +1000

    1 file modified

--- packages/backend/src/services/index.ts
+++ packages/backend/src/services/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { authRouter } from './auth';
 import { userRouter } from './users';
 
-export const services = express.Router();
+const services = express.Router();
 
 services.use('/auth', authRouter);
 services.use('/users', userRouter);
